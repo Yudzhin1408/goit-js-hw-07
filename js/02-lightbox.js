c9543705d668bc0f4e3d901ef9ecdf1593f6884b
Yevhenii Kononets <yudzhin1408@gmail.com>
2022-04-23 15:20:12 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -6,13 +6,15 @@ const galleryEl = document.querySelector('.gallery');
 
 
 const imagesEl = galleryItems.map(({ preview, original, description }) =>
-    `<a class="gallery__item" href="${original}">
+  `<li>
+    <a class="gallery__item" href="${original}">
   <img class="gallery__image" src="${preview}" alt="${description}" />
-</a>`
+</a>
+</li>`
 ).join('');
     
 galleryEl.insertAdjacentHTML('beforeend',imagesEl);
 
 // open
 let gallery = new SimpleLightbox('.gallery a', {captionsData: "alt", captionDelay:250,});
-gallery.on('show.simplelightbox');
+
